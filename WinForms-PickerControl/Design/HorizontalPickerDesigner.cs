656627b7850360dac46ea2cca003d4b85443e73f
Pedro F Linhares <pedro.fiuza@dellead.com>
2017-09-21 17:00:20 -0300

    Reorganized and followed a little more of the tutorial.

---
+++ WinForms-PickerControl/Design/HorizontalPickerDesigner.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Drawing.Design;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+
+namespace PickerControl.Design
+{
+    [ToolboxItemFilter("HorizontalPicker.MarqueeBorder", ToolboxItemFilterType.Require)]
+    [ToolboxItemFilter("HorizontalPicker.MarqueeText", ToolboxItemFilterType.Require)]
+    [System.Security.Permissions.PermissionSet(System.Security.Permissions.SecurityAction.Demand, Name = "FullTrust")]
+    class HorizontalPickerDesigner : DocumentDesigner
+    {
+
+    }
+}
